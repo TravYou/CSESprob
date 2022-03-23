3be660c3d23aa0408bc2c4a409638926fcd322f0
Travis You <zyou3@jh.edu>
2022-03-23 19:54:49 -0400

    finished increasingarr.cpp

---
+++ intro/increasingarr.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using std::cin;
+using std::cout;
+
+int main(){
+    int sz = 0;
+    cin >> sz;
+
+    long long max;
+    cin >> max;
+
+    long long step = 0;
+    long long temp;
+    for (int i = 1; i < sz; i++){
+        cin >> temp;
+        if (temp < max){
+            step += (max - temp);
+        }else if (temp > max){
+            max = temp;
+        }
+    }
+    cout << step;
+    return 0;
+}
